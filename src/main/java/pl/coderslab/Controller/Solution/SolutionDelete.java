a74da5b4e6105271fd129243b9e74d2f8e19c937
Michał Hordyjewicz <hordyjewiczmichal@gmail.com>
2018-10-29 11:14:30 +0100

    Usuwanie rozwiązań

--- src/main/java/pl/coderslab/Controller/Solution/SolutionDelete.java
+++ src/main/java/pl/coderslab/Controller/Solution/SolutionDelete.java
@@ -1,13 +1,19 @@
 package pl.coderslab.Controller.Solution;
 
+import pl.coderslab.Dao.SolutionDao;
+import pl.coderslab.Entity.Solution;
+import pl.coderslab.Entity.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet(name = "SolutionDelete",urlPatterns = {"/solutions/delete","/solutions/delete/"})
+@WebServlet(name = "SolutionDelete", urlPatterns = {"/solutions/delete", "/solutions/delete/"})
 public class SolutionDelete extends HttpServlet
 {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
@@ -17,6 +23,36 @@ public class SolutionDelete extends HttpServlet
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        response.getWriter().append("usuwanie"); // todo :: obsłużyć delete
+        HttpSession session = request.getSession(false);
+        SolutionDao solutionDao = new SolutionDao();
+
+        String id = request.getParameter("id");
+        Solution solution;
+        try
+        {
+            Integer intId = Integer.parseInt(id);
+            solution = solutionDao.getById(intId);
+
+            //sprawdz czy user ma uprawnienia do edycji (czyli do jego rozwiazanie)
+            User user = (User) session.getAttribute("session_user");
+            if (!(solution.getUser().getId().equals(user.getId())))
+            {
+                solution = null; // nie daję obiektu rozwiązania , bo ten user co jest w sesji nie dodał tego rozwiązania
+            }
+            solutionDao.delete(solution.getId()); // usuwam
+            request.setAttribute("table_info", "<span style='color: green;'>Pomyślnie usunięto.</span>");
+        }
+        catch (NumberFormatException | NullPointerException e) // jak błędne ?id podane
+        {
+            // złapie jak ?id nie jest liczbą (NFE) lub id nie istnieje (przy porównaniu if będzie NPE)
+            //solution = null;
+            request.setAttribute("table_info", "<span style='color: red;'>Rozwiązanie nie istnieje lub nie masz uprawnień do jego usunięcia.</span>");
+        }
+
+        User user = (User) session.getAttribute("session_user");
+        List<Solution> solutions = solutionDao.findAll(null, null, user.getId()); // do powrotu do user settings
+        request.setAttribute("solutions", solutions); // jw
+
+        getServletContext().getRequestDispatcher("/WEB-INF/views/user/settings.jsp").forward(request, response); // daj widok ustawień
     }
 }
--- src/main/webapp/WEB-INF/views/user/settings.jsp
+++ src/main/webapp/WEB-INF/views/user/settings.jsp
@@ -49,14 +49,42 @@ edycja danych usera................ <%-- todo :: edycja danych usera --%>
                             <a href="<c:url value="/exercise/${solution.exercise.title}?id=${solution.exercise.id}" />">${solution.exercise.title}</a>
                         </td>
                         <td>
-                            <a href="<c:url value="/solutions/edit?id=${solution.id}" />">Edytuj</a> / <a
-                                href="<c:url value="/solutions/delete?id=${solution.id}" />">Usuń</a>
+                            <a class="btn btn-primary btn-block"
+                               href="<c:url value="/solutions/edit?id=${solution.id}" />">Edytuj</a>
+                            <a class="btn btn-primary btn-block btn-delete"
+                               href="<c:url value="/solutions/delete?id=${solution.id}" />" data-toggle="modal"
+                               data-target="#deleteModal-${solution.id}">Usuń</a>
                         </td>
                     </tr>
+
+                    <!-- Delete Modal-->
+                    <div class="modal fade" id="deleteModal-${solution.id}" tabindex="-1" role="dialog"
+                         aria-labelledby="exampleModalLabel"
+                         aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Usunąć?</h5>
+                                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">×</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">Kliknij <i>Usuń</i>, aby trwale usunąć wybrane rozwiązanie.
+                                </div>
+                                <div class="modal-footer">
+                                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Anuluj</button>
+                                    <a class="btn btn-primary btn-delete"
+                                       href="<c:url value="/solutions/delete?id=${solution.id}" />">Usuń</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </c:forEach>
                 </tbody>
             </table>
         </div>
     </div>
 </div>
+
+
 <jsp:include page="./../template/footer.jsp"/>
